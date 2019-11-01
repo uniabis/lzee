cde578de37bb8e3e6de2357ea8db0d1d1a58305d
uniabis <uniabis@uniabis.net>
2019-11-01 22:54:48 +0900

    Updated dlzee_fast

--- dlzee_fast.asm
+++ dlzee_fast.asm
@@ -25,8 +25,8 @@
 ;
 
 dlze:
-		ld	a,080h
-		jr	dlze_lp1
+		ldi
+		scf
 
 getbit1:
 		ld	a,(hl)
@@ -68,7 +68,7 @@ dlze_copy:
 		jr	dlze_lp2
 
 dlze_far:
-		exa
+		ex      af, af';'
 		ld	a,(hl)
 		inc	hl
 		push	hl
@@ -93,7 +93,7 @@ dlze_far:
 dlze_skip:
 		ld	b,0
 		ld	c,a
-		exa
+		ex      af, af';'
 		jr	dlze_copy
 
 getbit:
